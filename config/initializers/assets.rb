da04fe28ff9f378965c628126100e6831d65674b
braian ocares <ocares.bra@gmail.com>
2023-06-01 13:56:25 -0300

    begingsWatchList

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
-
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
@@ -10,3 +10,8 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+gem "autoprefixer-rails"
+gem "font-awesome-sass", "~> 6.1"
+gem "simple_form"
+gem "sassc-rails"
