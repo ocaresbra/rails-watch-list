17b83af17728b164ac7535645d47394b79c021c0
braian ocares <ocares.bra@gmail.com>
2023-06-02 17:39:28 -0300

    just3left

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   has_many :lists, through: :bookmarks
 
-  validates :title, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
 end
